483807df2b29b909f026dc93e01d2e8da7ca1ec3
Yoga 7i <abhi290392@gmail.com>
2024-05-26 09:22:02 +0530

    Spark Context commit

--- src/main/java/com/example/spark/SparkContext.java
+++ src/main/java/com/example/spark/SparkContext.java
@@ -1,8 +1,6 @@
 package com.example.spark;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
 public class SparkContext{
